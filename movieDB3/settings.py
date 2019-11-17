2808acef88a2c4ba9ec0e12d9e370fafec09991c
koozzi <kchy666@naver.com>
2019-11-18 01:09:03 +0900

    tlqkfaskjfn

--- movieDB3/settings.py
+++ movieDB3/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '=uz70s&_yhrc2(o*0!c!4^d*#x$t0s4pdhisi=e-&c@5&!vxfu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kchy666.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','kchy666.pythonanywhere.com']
 
 # Application definition
 
@@ -71,25 +71,25 @@ WSGI_APPLICATION = 'movieDB3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kchy666$default',
-        'USER': 'kchy666',
-        'PASSWORD': 'rnclgns1!',
-        'HOST': 'kchy666.mysql.pythonanywhere-services.com',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'kchy666$default',
+#         'USER': 'kchy666',
+#         'PASSWORD': 'rnclgns1!',
+#         'HOST': 'kchy666.mysql.pythonanywhere-services.com',
+#         'OPTIONS': {
+#             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
